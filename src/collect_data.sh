27650d46d66e927a075e01c4fe2dc142731b0f4e
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-02-17 16:19:39 -0500

    Update collect_data.sh for parallel use on cluster

--- src/collect_data.sh
+++ src/collect_data.sh
@@ -10,19 +10,25 @@ size=512 # matrix size
 temp=`echo -n $file | wc -c`
 executable=`echo -n $file | head -c $(($temp-2))`
 
+parallel-ssh mkdir -p ~/gemm-raspberrypi-cluster/src/
+echo
+
 # compile and run program for 3 different sizes
 for i in {1..3}
 do
     size=$(($size*2))
     sed -i "s/#define LENGTH.*/#define LENGTH        $size/" ./$file
     make -s $executable
+    parallel-scp ./$executable ~/gemm-raspberrypi-cluster/src/  
+    echo
 
     echo results for $file \($size\x$size\)
 
     for j in {1..10}
     do
         echo -n $j\)" " 
-        ./$executable
+        #./$executable
+        mpirun ~/gemm-raspberrypi-cluster/src/$executable
         sleep 60
     done
 
