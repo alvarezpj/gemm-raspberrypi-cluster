3b7e19538a9af809568fb312361fdae2b3398e3d
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-05-17 19:31:40 -0400

    Update script

--- src/sample-collector-2.sh
+++ src/sample-collector-2.sh
@@ -3,8 +3,9 @@
 ######################################################################
 #                                                                    #
 # Bash script to collect sample data from (Raspberry Pi) cluster. It #
-# works by compiling and running the matrix multiplication program   #
-# "omp_mpi_neon.c" and changing the following three parameters:      #
+# works by compiling and running the matrix multiplication programs  #
+# "sequential.c", "omp.c", and "omp_mpi_neon.c" and changing the     #
+# following three parameters:                                        #
 #                                                                    #
 #     1. number of threads: 1, 2, 3, and 4                           #
 #     2. number of nodes: 1, 3, 6, 9, and 12                         #
@@ -15,15 +16,61 @@
 ######################################################################
 
 
-# set up variables
-directory=`pwd`
+# set up variables for "sequential.c"
+file="sequential.c"
+executable="sequential"
+msizes=(1024 2048 4096)
+
+# compile and execute program
+# loop structure: matrix sizes --> number of repetitions 
+for i in ${msizes[@]}
+do
+    sed -i "s/#define LENGTH.*/#define LENGTH        $i/" ./$file
+    make -s $executable
+    echo "Results corresponding to [nnodes=1, nthreads=1, msize=$i]"
+
+    for j in {1..10}
+    do
+        echo "trial $j"
+        ./sequential
+        sleep 120
+    done
+done
+
+# set up variables for "omp.c"
 header="mxmultiply.h"
+file="omp.c"
+executable="omp"
+
+# compile and execute program
+# loop structure: number of threads --> matrix sizes --> number of repetitions 
+for i in {2..4}
+do
+    sed -i "s/#define NUMTHREADS.*/#define NUMTHREADS                             $i/" ./$header
+
+    for j in ${msizes[@]}
+    do
+        sed -i "s/#define LENGTH.*/#define LENGTH        $j/" ./$file
+        make -s $executable
+        echo -e "\nResults corresponding to [nnodes=1, nthreads=$i, msize=$j]"
+
+        for k in {1..10}
+        do
+            echo "trial $k"
+            ./$executable
+            sleep 120
+        done 
+    done
+done 
+
+# set up variables for "omp_mpi_neon.c"
+directory=`pwd`
 file="omp_mpi_neon.c"
 executable="omp_mpi_neon"
 nnodes=(3 6 9 12)
-msizes=(1024 2048 4096)
 
 # create directories in slave nodes
+echo
 parallel-ssh -i -h ~/.ssh/hosts_file mkdir -p $directory
 
 # compile and execute program
@@ -44,7 +91,7 @@ do
 
             for l in {1..10}
             do
-                echo -n "$l. "
+                echo "trial $l"
                 mpirun -np $i -machinefile ~/.mpiconfig/nodes $directory/$executable 
                 sleep 120
             done 
